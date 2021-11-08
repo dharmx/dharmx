4b242add497b354b7600b2e1d714fc5f0c77cb2d
Keym♔ster <80379926+pagankeymaster@users.noreply.github.com>
2021-11-08 09:37:49 +0530

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,4 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
-export LD_PRELOAD=/usr/lib/libwcwidth-icons.so
+# export LD_PRELOAD=/usr/lib/libwcwidth-icons.so
 export VISUAL=nvim;
 export EDITOR=nvim;
 export PATH="/home/maker/.local/share/gem/ruby/3.0.0/bin:/home/maker/.jdks/jdk-16.0.1/bin:/home/maker/.bin:/home/maker/.local/bin:$PATH"
@@ -14,6 +10,8 @@ alias lla="logo-ls -l --all"
 
 alias mount-phone "go-mtpfs ~/phone"
 alias umount-phone "fusermount -u ~/phone"
+alias mount-ssd="udisksctl mount -b /dev/nvme0n1p1"
+alias unmount-ssd="udisksctl unmount -b /dev/nvme0n1p1"
 
 alias logout "killall bspwm"
 alias lock "betterlockscreen -l dim --off 5"
@@ -29,13 +27,10 @@ alias nf "neofetch --source (~/.config/neofetch/randomize.sh ~/.config/neofetch/
 alias pf "neofetch --config /home/maker/.config/neofetch/config-minimal.conf --ascii ~/.config/neofetch/arch-smol"
 alias uwuf "neofetch --config /home/maker/.config/neofetch/config-minimal-uwu.conf --ascii ~/.config/neofetch/uwu-hd"
 
-alias pipes1 "~/Templates/stark-color-scripts/color-scripts/pipes1"
-alias pipes2 "~/Templates/stark-color-scripts/color-scripts/pipes2"
-alias pipes2-slim "~/Templates/stark-color-scripts/color-scripts/pipes2-slim"
-alias rain "~/Templates/custom-color-scripts/rain"
-
-# wal -q --theme base16-nordocean
-# wal -q --theme base16-dale
+alias pipes1 "/home/maker/Dotfiles/lambda-scripts/aesthetic/pipes1"
+alias pipes2 "/home/maker/Dotfiles/lambda-scripts/aesthetic/pipes2"
+alias pipes2-slim "/home/maker/Dotfiles/lambda-scripts/aesthetic/pipes2-slim"
+alias rain "/home/maker/Dotfiles/lambda-scripts/aesthetic/rain"
 
 clear
 colorscript exec alpha
