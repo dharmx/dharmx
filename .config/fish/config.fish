737bec647a380e198962420500fd108486fbd113
Keym♔ster <80379926+pagankeymaster@users.noreply.github.com>
2021-11-08 09:39:52 +0530

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,20 +10,19 @@ alias lla="logo-ls -l --all"
 
 alias mount-phone "go-mtpfs ~/phone"
 alias umount-phone "fusermount -u ~/phone"
-alias mount-ssd="udisksctl mount -b /dev/nvme0n1p1"
-alias unmount-ssd="udisksctl unmount -b /dev/nvme0n1p1"
+alias mount-ssd "udisksctl mount -b /dev/nvme0n1p1"
+alias unmount-ssd "udisksctl unmount -b /dev/nvme0n1p1"
 
 alias logout "killall bspwm"
 alias lock "betterlockscreen -l dim --off 5"
 alias suspend "systemctl suspend"
 
 alias pac "sudo pacman"
-# alias vi "vim"
 alias nv "nvim"
 alias fm "ranger"
 
 alias nf "neofetch --source (~/.config/neofetch/randomize.sh ~/.config/neofetch/paths.txt) --backend w3m --loop"
-# alias nfk "neofetch --source (~/.config/neofetch/randomize.sh ~/.config/neofetch/paths.txt) --backend kitty"
+alias nfk "neofetch --source (~/.config/neofetch/randomize.sh ~/.config/neofetch/paths.txt) --backend kitty"
 alias pf "neofetch --config /home/maker/.config/neofetch/config-minimal.conf --ascii ~/.config/neofetch/arch-smol"
 alias uwuf "neofetch --config /home/maker/.config/neofetch/config-minimal-uwu.conf --ascii ~/.config/neofetch/uwu-hd"
 
