eaf883d890f0daebfe09bef7f9ed2c022b1872d2
pagankeymaster <80379926+pagankeymaster@users.noreply.github.com>
2021-12-13 03:11:22 +0530

    minor changes

---
+++ config/sxhkd/scripts/important-marker.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+XRDB_BRIGHTBLUE="$(xrdb -query | grep color12 | awk '{print $2}')"
+
+if [[ "$(bspc config -n focused focused_border_color)" == "$XRDB_BRIGHTBLUE" ]]; then
+  bspc config -n focused focused_border_color "#bf616a"
+  bspc config -n focused border_width 10
+  bspc node focused --flag marked=on
+else 
+  bspc config -n focused focused_border_color "$XRDB_BRIGHTBLUE"
+  bspc config -n focused border_width 3
+  bspc node focused --flag marked=off
+fi
+
