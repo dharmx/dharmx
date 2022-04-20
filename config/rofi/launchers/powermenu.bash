1735232f57fc0ff70b00b195aca49f882da9652e
pagankeymaster <80379926+pagankeymaster@users.noreply.github.com>
2022-04-20 11:45:18 +0530

    added nvim submodule
    added nvim submodule
    
    chore

--- config/rofi/launchers/powermenu.bash
+++ config/rofi/launchers/powermenu.bash
@@ -39,7 +39,7 @@ confirm_exit() {
 
 # Message
 msg() {
-    rofi -theme "$XDG_CONFIG_HOME/rofi/configs/message.rasi" -e "Available Options  -  yes / y / no / n"
+    rofi -theme "$XDG_CONFIG_HOME/rofi/configs/message.rasi" -e "Available Options  -  Yup / Nope"
 }
 
 # Variable passed to rofi
