5694ab38d61dd938dcd2d9c78b056ca779fbea03
pagankeymaster <80379926+pagankeymaster@users.noreply.github.com>
2022-07-05 20:07:09 +0530

    feat: added dual-border script

--- config/_main/bspwm/extern.bash
+++ config/_main/bspwm/extern.bash
@@ -8,7 +8,7 @@ consequences="$4"
 
 function main() {
   case "$class" in
-    Zettlr) 
+    Zettlr)
       local wm_name
       wm_name=$(xprop -id "$window" | grep '_NET_WM_NAME' | grep --perl-regexp --only-matching '"\K[^,"]+')
       [ "$wm_name" = Preferences ] && echo "rectangle=926x724+493+175 center=on state=floating"
@@ -18,8 +18,9 @@ function main() {
     FromScratch) echo 'center=off state=floating rectangle=440x415+1435+40 sticky=on';;
     '')
       sleep 0.5
-      local wm_class=($(xprop -id $window | grep 'WM_CLASS' | grep --perl-regexp --only-matching '"\K[^,"]+'))
-      local class=${wm_class[-1]}
+      local wm_class class
+      wm_class=($(xprop -id $window | grep 'WM_CLASS' | grep --perl-regexp --only-matching '"\K[^,"]+'))
+      class=${wm_class[-1]}
       [ ${#wm_class[@]} -eq 2 ] && instance=${wm_class[0]}
       [ -n "$class" ] && main
     ;;
